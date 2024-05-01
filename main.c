627cb1a30d2b8d4638d5aeabe21b556853e756da
Nick Cao <nickcao@nichi.co>
2024-04-30 22:46:46 -0400

    properly handle error

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
-#include <errno.h>
 #include <fcntl.h>
 #include <linux/rfkill.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
 int main() {
   int fd = open("/dev/rfkill", O_RDONLY);
-  if (fd < 0)
-    return errno;
+  if (fd < 0) {
+    perror("failed to open /dev/rfkill");
+    exit(EXIT_FAILURE);
+  }
 
   int r = ioctl(fd, RFKILL_IOCTL_NOINPUT);
-  if (r < 0)
-    return r;
+  if (r < 0) {
+    perror("failed to execute ioctl");
+    exit(EXIT_FAILURE);
+  }
 
   pause();
 
-  return 0;
+  exit(EXIT_SUCCESS);
 }
